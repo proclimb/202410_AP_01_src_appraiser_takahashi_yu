4eb8dd8814ae418edce58edd1f258057b785370e
yu takahashi <202410ap01_y.takahashi@x-ide.sakura.ne.jp>
2025-01-16 14:11:57 +0900

    #32122　「距離」初期値：Aにチェックなし

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -243,7 +243,7 @@ function subStockEditView($param)
 		<table border="0" cellpadding="5" cellspacing="1">
 			<tr>
 				<th>除外</th>
-				<td><input type="radio" name="del" value="1" /> 非除外
+				<td><input type="radio" name="del" value="1" checked="checked" /> 非除外
 					<input type="radio" name="del" value="0" /> 除外
 				</td>
 			</tr>
@@ -289,7 +289,7 @@ function subStockEditView($param)
 					<?php
 					for ($i = 0; $i < 4; $i++) {
 					?>
-						<input type="radio" name="distance" value="<?php print $i + 1; ?>" <?php if ($param["distance"] == $i + 1) print ' checked="checked"'; ?> /> <?php print fnDistanceName($i); ?>
+						<input type="radio" name="distance" value="<?php print $i + 1; ?>" <?php if ($param["distance"] == $i) print ' checked="checked"'; ?> /> <?php print fnDistanceName($i); ?>
 					<?php
 					}
 					?>
