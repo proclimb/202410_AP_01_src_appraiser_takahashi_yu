c1834735264d5ebf291df20fd1186466d74c5075
yu takahashi <202410ap01_y.takahashi@x-ide.sakura.ne.jp>
2025-01-27 14:44:58 +0900

    #32192 更新画面にて、登録されている「仕入経緯」が選択されていない

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -342,10 +342,18 @@ function subStockEditView($param)
 				<th>仕入経緯</th>
 				<td>
 					<?php
+
+					if (!$param["stockNo"]) {
+						$param["how"] = 1;
+					}
 					for ($i = 0; $i < 6; $i++) {
+						$check = '';
+						if (($i + 1) == $param["how"]) {
+							$check = 'checked = "checked"';
+						}
 					?>
 						<br />
-						<input type="radio" name="how" value="<?php print $i + 1; ?>" <?php if ($param["how"] == $i) print ' checked="checked"'; ?> /> <?php print fnHowName($i); ?>
+						<input type="radio" name="how" value="<?php print $i + 1; ?>" <?php print $check; ?> /> <?php print fnHowName($i); ?>
 					<?php
 					}
 					?>
