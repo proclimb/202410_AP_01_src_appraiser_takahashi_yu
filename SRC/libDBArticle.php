6233ab7c1e5a069cbf87f827740dac8f9d317bf9
yu takahashi <202410ap01_y.takahashi@x-ide.sakura.ne.jp>
2025-02-18 12:20:59 +0900

    #35852 「キーBOX」に値を入力して検索押下→検索されない

--- SRC/libDBArticle.php
+++ SRC/libDBArticle.php
@@ -15,7 +15,7 @@ function fnSqlArticleList($flg, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleN
 	$sql .= " FROM TBLARTICLE";
 	$sql .= " WHERE DEL = $sDel";
 	if ($sArticle) {
-		$sql .= " AND ARTICLE LIKE '%$sArticle$%'";
+		$sql .= " AND ARTICLE LIKE '%$sArticle%'";
 	}
 	if ($sRoom) {
 		$sql .= " AND ROOM LIKE '%$sRoom%'";
@@ -27,7 +27,7 @@ function fnSqlArticleList($flg, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleN
 		$sql .= " AND ARTICLENOTE LIKE '%$sArticleNote%'";
 	}
 	if ($sKeyBox) {
-		$sql .= " AND KEYBOX LIKE '%l$sKeyBox%'";
+		$sql .= " AND KEYBOX LIKE '%$sKeyBox%'";
 	}
 	if ($sDrawing) {
 		$sql .= " AND DRAWING LIKE '%$sDrawing%'";
